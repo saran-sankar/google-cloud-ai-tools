5a230a98f2da5c7b74967b60fb09f8f5c1ba5873
Saran Sankar <epsaransankar@gmail.com>
2022-05-08 18:51:47 +0530

    Created tool for batch prediction

---
+++ examples/create-batch-reqest-input/create_batch_reqest_input.py
@@ -0,0 +1,27 @@
+import os
+import json
+import random
+
+"""
+Program to create JSON input files for batch prediction for images stored in a folder.
+"""
+
+bucket_name = ""
+
+json_list = []
+
+#Input bucket name and name of the folder containing the batch
+batch_path = input("\nURI of the batch directory (starting with the bucket name, e.g: bucket/batch): ")
+folder_name = input("\nName of the local folder containing the batch (must be in the same directory as the python file): ")
+
+#Create input JSON file
+image_files = [name for name in os.listdir("./{}".format(folder_name))]
+
+for image_file_name in image_files:
+
+    image_type = "jpg"
+    json_line = {"content": "gs://{}/{}".format(batch_path, image_file_name), "mimeType": "image/{}".format(image_type)}
+    json_list.append(json.dumps(json_line))
+
+with open('image_classification_batch.jsonl', 'w') as f:
+    f.write("\n".join(json_list))
